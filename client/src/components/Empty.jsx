8b7e1d7eb0a2bb29eb492ee74bddf89fe067de67
Farzana Asadi <121952162+Dawlatshahi@users.noreply.github.com>
2024-05-20 19:13:12 +0300

    empty fixed

--- client/src/components/Empty.jsx
+++ client/src/components/Empty.jsx
@@ -4,7 +4,7 @@ import { IoLogoWechat } from 'react-icons/io5';
 
 function Empty() {
 	return (
-		<div className="border-conversation-border border-l z-10 w-full bg-panel-header-background flex flex-col h-[100vh] border-b-4 border-b-empty-border items-center justify-center sm:w-screen">
+		<div className="border-conversation-border border-l  h-[100vh]  w-full flex flex-col  z-10 bg-panel-header-background  border-b-4 border-b-empty-border  md:w-full sm:w-screen items-center justify-center">
 			<span className="text-gray-300">Select a chat to start messaging</span>
 			<IoLogoWechat className="text-gray-400 h-32 w-auto mt-4" />
 		</div>
