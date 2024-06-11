c9c1078cdcd0119813ba0d59f3499257fbcdba9c
Farzana Asadi <121952162+Dawlatshahi@users.noreply.github.com>
2024-06-11 16:22:42 +0300

    Today's messages header added.

--- client/src/components/Chat/ChatContainer.jsx
+++ client/src/components/Chat/ChatContainer.jsx
@@ -12,6 +12,16 @@ const VoiceMessage = dynamic(() => import('@/components/Chat/VoiceMessage'), {
 	ssr: false,
 });
 
+const isToday = (date) => {
+	const today = new Date();
+	const messageDate = new Date(date);
+	return (
+		today.getDate() === messageDate.getDate() &&
+		today.getMonth() === messageDate.getMonth() &&
+		today.getFullYear() === messageDate.getFullYear()
+	);
+};
+
 export default function ChatContainer() {
 	const [{ messages, currentChatUser, userInfo, dispatch }] =
 		useStateProvider();
@@ -120,7 +130,7 @@ export default function ChatContainer() {
 					...prevDeletedMessages,
 					[messageId]: true,
 				}));
-				toast.success('Message deleted successfully!', { autoClose: 2000 }); // 2 seconds
+				toast.success('Message deleted successfully!', { autoClose: 2000 });
 			} else {
 				const errorMessage = await response.text();
 				toast.error(`Error deleting message: ${errorMessage}`, {
@@ -128,7 +138,7 @@ export default function ChatContainer() {
 				});
 			}
 		} catch (error) {
-			toast.error(`Network error: ${error.message}`, { autoClose: 2000 }); // 2 seconds
+			toast.error(`Network error: ${error.message}`, { autoClose: 2000 });
 		}
 	};
 
@@ -162,91 +172,112 @@ export default function ChatContainer() {
 			<div className="mx-10 my-6 relative bottom-0 z-40 left-0">
 				<div className="flex w-full">
 					<div className="flex flex-col justify-end w-full gap-1 overflow-auto">
-						{displayedMessages.map((message, index) => (
-							<div
-								key={index}
-								className={`flex ${
-									message.senderId === currentChatUser.id
-										? 'justify-start'
-										: 'justify-end'
-								}`}
-							>
-								{!deletedMessages[message.id] && (
-									<>
-										{message.type === 'text' && (
-											<div
-												className={`text-white px-2 py-[2px] text-sm rounded-md flex flex-col gap-1 items-start max-w-[70%] overflow-x-hidden relative dark:text-white ${
-													message.senderId === currentChatUser.id
-														? 'bg-incoming-background dark:bg-gray-500'
-														: 'bg-outgoing-background dark:bg-gray-700'
-												}`}
-											>
-												{message.senderId === userInfo.id && (
-													<button
-														onClick={() =>
-															confirmAndDeleteMessage(message.id, message.type)
-														}
-														className="text-gray-300 text-xs ml-auto mt-2 flex items-center p-1"
+						{displayedMessages.map((message, index) => {
+							const showDateHeader =
+								index === 0 ||
+								(isToday(message.createdAt) &&
+									!isToday(displayedMessages[index - 1]?.createdAt));
+							return (
+								<div key={index}>
+									{showDateHeader && (
+										<div className="text-center text-gray-400 text-xs my-2">
+											{isToday(message.createdAt) ? 'Today' : 'All Messages'}
+										</div>
+									)}
+									<div
+										className={`flex ${
+											message.senderId === currentChatUser.id
+												? 'justify-start'
+												: 'justify-end'
+										}`}
+									>
+										{!deletedMessages[message.id] && (
+											<>
+												{message.type === 'text' && (
+													<div
+														className={`text-white px-2 py-[2px] text-sm rounded-md flex flex-col gap-1 items-start max-w-[70%] overflow-x-hidden relative dark:text-white ${
+															message.senderId === currentChatUser.id
+																? 'bg-incoming-background dark:bg-gray-500'
+																: 'bg-outgoing-background dark:bg-gray-700'
+														}`}
 													>
-														<BsTrash size={14} className="cursor-pointer" />
-													</button>
-												)}
-												<span className="break-all">{message.message}</span>
-												<div className="flex gap-2 ml-auto">
-													<span className="text-bubble-meta text-[11px] items-end">
-														{calculateTime(message.createdAt)}
-													</span>
-													{message.senderId === userInfo.id && (
-														<>
-															<span className="items-center">
-																<MessageStatus
-																	messageStatus={message.messageStatus}
-																/>
+														{message.senderId === userInfo.id && (
+															<button
+																onClick={() =>
+																	confirmAndDeleteMessage(
+																		message.id,
+																		message.type
+																	)
+																}
+																className="text-gray-300 text-xs ml-auto mt-2 flex items-center p-1"
+															>
+																<BsTrash size={14} className="cursor-pointer" />
+															</button>
+														)}
+														<span className="break-all">{message.message}</span>
+														<div className="flex gap-2 ml-auto">
+															<span className="text-bubble-meta text-[11px] items-end">
+																{calculateTime(message.createdAt)}
 															</span>
-														</>
-													)}
-												</div>
-											</div>
-										)}
-										{message.type === 'image' && (
-											<div className="relative">
-												<ImageMessage message={message} />
-												{message.senderId === userInfo.id && (
-													<div className=" bg-gray-500 ">
-														<button
-															onClick={() =>
-																confirmAndDeleteMessage(
-																	message.id,
-																	message.type
-																)
-															}
-															className="absolute top-2 right-2 text-gray-300 text-xs ml-2 flex items-center bg-outgoing-background p-1 rounded"
-														>
-															<BsTrash size={14} className="cursor-pointer" />
-														</button>
+															{message.senderId === userInfo.id && (
+																<>
+																	<span className="items-center">
+																		<MessageStatus
+																			messageStatus={message.messageStatus}
+																		/>
+																	</span>
+																</>
+															)}
+														</div>
 													</div>
 												)}
-											</div>
-										)}
-										{message.type === 'audio' && (
-											<div className="relative">
-												<VoiceMessage message={message} />
-												{message.senderId === userInfo.id && (
-													<button
-														onClick={() =>
-															confirmAndDeleteMessage(message.id, message.type)
-														}
-														className="absolute top-2 right-2 text-gray-300 text-xs ml-1 flex items-center p-1"
-													>
-														<BsTrash size={14} className="cursor-pointer" />
-													</button>
+												{message.type === 'image' && (
+													<div className="relative">
+														<ImageMessage message={message} />
+														{message.senderId === userInfo.id && (
+															<div className=" bg-gray-500 ">
+																<button
+																	onClick={() =>
+																		confirmAndDeleteMessage(
+																			message.id,
+																			message.type
+																		)
+																	}
+																	className="absolute top-2 right-2 text-gray-300 text-xs ml-2 flex items-center bg-outgoing-background p-1 rounded"
+																>
+																	<BsTrash
+																		size={14}
+																		className="cursor-pointer"
+																	/>
+																</button>
+															</div>
+														)}
+													</div>
 												)}
-											</div>
+												{message.type === 'audio' && (
+													<div className="relative">
+														<VoiceMessage message={message} />
+														{message.senderId === userInfo.id && (
+															<button
+																onClick={() =>
+																	confirmAndDeleteMessage(
+																		message.id,
+																		message.type
+																	)
+																}
+																className="absolute top-2 right-2 text-gray-300 text-xs ml-1 flex items-center p-1"
+															>
+																<BsTrash size={14} className="cursor-pointer" />
+															</button>
+														)}
+													</div>
+												)}
+											</>
 										)}
-									</>
-								)}
-							</div>
-						))}
+									</div>
+								</div>
+							);
+						})}
 					</div>
 				</div>
 			</div>
